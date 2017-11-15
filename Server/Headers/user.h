455dcf34fd2ed5ad8e9f5dd338988252a9455ddc
Bruno Zimpel <brunozimpel@gmail.com>
2017-11-15 00:11:42 -0200

    Separate the headers and the sources

--- Server/Headers/user.h
+++ Server/Headers/user.h
@@ -1,20 +1,69 @@
-#include <string>
-//using namespace std;
+//#include <string>
+#include <iostream>
+using namespace std;
 
-class User
+class user
 {
      private:
      int id;
-     int contact;
-     int period;
-     std:string name;
+     int num;
+    // string mail;
+     //Days:
+     int monEnter;
+     int monExit;
+     int tueEnter;
+     int tueExit;
+     int wedEnter;
+     int wedExit;
+     int thuEnter;
+     int thuExit;
+     int friEnter;
+     int friExit;
+     int satEnter;
+     int satExit;
+     int sunEnter;
+     int sunExit;
+
+     std::string name;
      public:
      void setId(int id);
-     void setContact(int contact);
-     void setPeriod(int period);
-     void setName(std:string name);
+     void setNum(int num);
+    // void setMail(string mail);
+     //Set period in and out
+     void setMonEnter(int monEnter);
+     void setMonExit(int monExit);
+     void setTueEnter(int tueEnter);
+     void setTueExit(int tueExit);
+     void setWedEnter(int wedEnter);
+     void setWedExit(int wedExit);
+     void setThuEnter(int thuEnter);
+     void setThuExit(int thuExit);
+     void setFriEnter(int friEnter);
+     void setFriExit(int friExit);
+     void setSatEnter(int satEnter);
+     void setSatExit(int satExit);
+     void setSunEnter(int sunEnter);
+     void setSunExit(int sunExit);
+     
+     void setName(std::string name);
      int getId();
-     int getContact();
-     int getPeriod();
-	 std:string getName();
+     int getNum();
+  //   string getMail();
+     //Get period in and out
+     int getMonEnter();
+     int getMonExit();
+     int getTueEnter();
+     int getTueExit();
+     int getWedEnter();
+     int getWedExit();
+     int getThuEnter();
+     int getThuExit();
+     int getFriEnter();
+     int getFriExit();
+     int getSatEnter();
+     int getSatExit();
+     int getSunEnter();
+     int getSunExit();
+     
+     std::string getName();
 };
