e402af2ce40e6d3e20cf38b7c6675f717623f207
GLOFonseca <guilofonseca@gmail.com>
2017-11-21 23:57:02 -0200

    Manage Database with C++

---
+++ Database-manager/database_manager.cpp
@@ -0,0 +1,15 @@
+#include "table.cpp"
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <pqxx/pqxx>
+
+using namespace std;
+
+
+void main (string tokenized_input ) {
+        char * splitted_str;
+
+        splitted_str = strtok (tokenized_input, ",");
+
+}
